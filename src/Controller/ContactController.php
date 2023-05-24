497d20a3b20d5c3274ee6e991b1815fef6a0e2ff
Fédi <fedielmanoubi@gmail.com>
2023-05-24 17:00:32 +0200

    video 21fini - debut video final

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -31,4 +31,24 @@ class ContactController extends AbstractController
              'leContact' => $contact
          ]);
      }
+
+
+             /**
+     * @Route("/contact/sexe/{sexe}", name="listeContactsSexe", methods={"GET"})
+     */
+
+     public function listeContactsSexe($sexe, ContactRepository $repo)
+     {
+        // $Contacts=$repo->findBy(
+        //     ['sexe' => $sexe],
+        //     ['nom' => 'ASC']
+        // );
+        $Contacts=$repo->findBySexe($sexe);
+         return $this->render('contact/listeContacts.html.twig',[
+             'lesContacts' => $Contacts
+         ]);
+
+
+         
+        }
 }
